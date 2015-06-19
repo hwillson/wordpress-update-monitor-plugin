d9423d2dc6840ef7924ebf0002220fa77025bbbf
hwillson <hugh@octonary.com>
2015-06-19 06:32:06 -0400

    Initial checkin.

---
+++ update_monitor.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name: Update Monitor
+ * Plugin URI: TODO
+ * Description: Wordpress plugin that provides a REST based API for monitoring plugin update details.
+ * Version: 0.0.1
+ * Author: Octonary Inc.
+ * Author URI: http://octonary.com
+ * License: MIT
+ */
